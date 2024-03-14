eb7a961d080440780040980f6b77e968b617d712
Owais <owaisikraam65@gmail.com>
2024-03-14 09:37:30 +0500

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+let num1 = prompt("enter your 1st num");
+let num2 = prompt("enter yuor 2nd num");
+let result =num1*num2
+alert(result)
+
+let num3 = prompt("enter your 1st num");
+let num4 = prompt("enter yuor 2nd num");
+let result1 =num3-num4
+alert(result1)
+
+let num5 = Number(prompt("enter your 1st num"));
+let num6 = Number(prompt("enter yuor 2nd num"));
+let result2 =num5+num6
+alert(result2)
+
+let num7 = prompt("enter your 1st num");
+let num8 = prompt("enter yuor 2nd num");
+let result3 =num7/num8
+alert(result3)
